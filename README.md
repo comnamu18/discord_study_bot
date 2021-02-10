33d9e3704c12afae9e14454953d41f1de820775f
comnamu18 <comnamu18@gmail.com>
2021-02-10 19:26:23 +0900

    Update message's current time

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # discord_study_bot
 Discord 랜선 각자 모여 코딩용 bot
 
+
 ## Reference
 [Python으로 Discord Bot 만들기 Tutorial](https://realpython.com/how-to-make-a-discord-bot-python/)
+
 [Python 기반 Discord Bot](https://github.com/team-play-together/together-bot)
--- bot.py
+++ bot.py
@@ -15,18 +15,18 @@ async def on_ready():
 
 @bot.command(name='hi')
 async def hi(ctx):
-    await ctx.send(f'안녕하세요 {ctx.author.name}님!')
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    await ctx.send(f'안녕하세요 name:{ctx.author.name} display_name: {ctx.author.display_name()}님! : {current_time}')
     
 @bot.command(name='bye')
 async def bye(ctx):
-    await ctx.send(f'안녕히가세요 {ctx.author.name}님!')
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    await ctx.send(f'안녕히가세요 name:{ctx.author.name} display_name: {ctx.author.display_name()}님! : {current_time}')
 
 @bot.event
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
         if event == 'on_message':
             f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
 
 bot.run(TOKEN)
