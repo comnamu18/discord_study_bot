d7631d691a3e1976c2454b7f2569fa913db0d467
Manny <yjm9554@gmail.com>
2021-07-08 13:25:57 +0900

    문자열 겹따옴표로 통일

--- main.py
+++ main.py
@@ -7,36 +7,37 @@ from utils.handle_db import *
 from utils.handle_time import *
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
-db_name = os.getenv('DISCORD_DB')
-tb_name = os.getenv('DISCORD_DB_TB')
-admin_name = os.getenv('DISCORD_ADMIN')
+token = os.getenv("DISCORD_TOKEN")
+db_name = os.getenv("DISCORD_DB")
+tb_name = os.getenv("DISCORD_DB_TB")
+admin_name = os.getenv("DISCORD_ADMIN")
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
     init_db(db_name, tb_name)
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
 
-@bot.command(name='hi')
+@bot.command(name="hi")
 async def hi(ctx):
     current_time = get_current_time()
     start_study(ctx.author.name, tb_name, current_time)
-    await ctx.send(f'{current_time} : 안녕하세요 {ctx.author.name}님!')
+    await ctx.send(f"{current_time} : 안녕하세요 {ctx.author.name}님!")
+
     
-@bot.command(name='bye')
+@bot.command(name="bye")
 async def bye(ctx):
     current_time = get_current_time()
     end_study(ctx.author.name, tb_name, current_time)
-    await ctx.send(f'{current_time} :안녕히가세요 {ctx.author.name}님!')
+    await ctx.send(f"{current_time} :안녕히가세요 {ctx.author.name}님!")
 
-@bot.command(name='list')
+@bot.command(name="list")
 async def listing(ctx):
     list_all_from_db = list_study(tb_name)
     await ctx.send(list_all_from_db)
 
-@bot.command(name='exit')
+@bot.command(name="exit")
 async def exitBot(ctx):
     if ctx.author.name == admin_name:
         close_db()
@@ -47,9 +48,9 @@ async def exitBot(ctx):
 
 @bot.event
 async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
+    with open("err.log", "a") as f:
+        if event == "on_message":
+            f.write(f"Unhandled message: {args[0]}\n")
         else:
             raise
 
--- utils/handle_time.py
+++ utils/handle_time.py
@@ -2,8 +2,8 @@ from urllib import request
 from datetime import datetime, timedelta 
 
 def get_current_time():
-    str_time = request.urlopen('https://www.naver.com').headers['Date']
-    current_time = datetime.strptime(str_time,'%a, %d %b %Y %H:%M:%S %Z') - timedelta(hours=-9) 
+    str_time = request.urlopen("https://www.naver.com").headers["Date"]
+    current_time = datetime.strptime(str_time, "%a, %d %b %Y %H:%M:%S %Z") - timedelta(hours=-9)
     return current_time.strftime("%Y-%m-%d %H:%M:%S")
 
 def calculate_elapsed(start_time, end_time):
@@ -17,14 +17,14 @@ def handle_timedelta(td):
     hours = td.seconds//3600
     minutes = (td.seconds//60)%60
     seconds = td.seconds % 60
-    return_string = ''
+    return_string = ""
     if days != 0:
-        return_string += f'{days}일 '
+        return_string += f"{days}일 "
     if hours != 0:
-        return_string += f'{hours}시간 '
+        return_string += f"{hours}시간 "
     if minutes != 0:
-        return_string += f'{minutes}분 '
+        return_string += f"{minutes}분 "
     if seconds != 0:
-        return_string += f'{seconds}초'
+        return_string += f"{seconds}초"
 
     return return_string
