ec2b9a6fb930b66dfb8a0d011ace29a7cedbdfe4
Manny <yjm9554@gmail.com>
2021-07-09 15:26:27 +0900

    mysql에 대응하도록 varchar값 지정, 객체 생성시 기본값 할당

--- utils/User.py
+++ utils/User.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
 
-    user_nm = Column("USER_NM", String, primary_key=True)
+    user_nm = Column("USER_NM", String(20), primary_key=True)
     std_amt = Column("STD_AMT", Integer)
     std_cnt = Column("STD_CNT", Integer)
     last_login_hms = Column("LAST_LOGIN_HMS", TIMESTAMP, nullable=True)
@@ -15,6 +15,8 @@ class User(Base):
 
     def __init__(self, user_nm):
         self.user_nm = user_nm
+        self.std_amt = 0
+        self.std_cnt = 0
 
     def __str__(self):
         return f"user_nm={self.user_nm}, std_amt={self.std_amt}, std_cnt={self.std_cnt}, " \
