32ba5ffeac5b81d50a53457896aa644bff9af868
Manny <yjm9554@gmail.com>
2021-07-30 21:08:49 +0900

    discord.py의 user.id를 사용하도록 테이블 변경

--- tables/study_log.py
+++ tables/study_log.py
@@ -10,13 +10,16 @@ class StudyLog(Base):
     __tablename__ = "studyLog"
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    user_name = Column(String, ForeignKey(User.name))
+    user_id = Column(Integer, ForeignKey(User.id))
+    user_name = Column(String(20), ForeignKey(User.name))
     start_time = Column(TIMESTAMP)
     end_time = Column(TIMESTAMP, nullable=True)
 
-    def __init__(self, user_name, start_time):
+    def __init__(self, user_id, user_name, start_time):
+        self.user_id = user_id
         self.user_name = user_name
         self.start_time = start_time
 
     def __str__(self):
-        return f"id: {self.id}, 이름: {self.user_name}, 시작 시각: {self.start_time}, 종료 시각: {self.end_time}"
+        return f"id: {self.id}, 아이디: {self.user_id}, 이름: {self.user_name}, " \
+               f"시작 시각: {self.start_time}, 종료 시각: {self.end_time}"
--- tables/user.py
+++ tables/user.py
@@ -8,17 +8,19 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user"
 
-    name = Column(String(20), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
     study_amount = Column(Integer)
     study_count = Column(Integer)
     last_start_time = Column(TIMESTAMP, nullable=True)
     last_end_time = Column(TIMESTAMP, nullable=True)
 
-    def __init__(self, user_name):
+    def __init__(self, user_id, user_name):
+        self.id = user_id
         self.name = user_name
         self.study_amount = 0
         self.study_count = 0
 
     def __str__(self):
-        return f"유저명: {self.name}, 총 공부시간: {self.study_amount}, 총 공부횟수: {self.study_count}, " \
-               f"마지막 로그인 시각: {self.last_start_time}, 마지막 로그아웃 시각: {self.last_end_time}"
+        return f"유저아이디: {self.id}, 유저명: {self.name}, 총 공부시간: {self.study_amount}, 총 공부횟수: {self.study_count}," \
+               f" 마지막 로그인 시각: {self.last_start_time}, 마지막 로그아웃 시각: {self.last_end_time}"
