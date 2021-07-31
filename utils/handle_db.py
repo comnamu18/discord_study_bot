428794156745d2015d8bc3a1e56777ea761e05dc
Kim SangHeon <comnamu18@gmail.com>
2021-07-31 22:54:42 +0900

    Revert "Update Github Action (#10)"
    This reverts commit 91fcd23714517a6a2428f3dcd414ad99be588049.

--- utils/handle_db.py
+++ utils/handle_db.py
@@ -4,7 +4,6 @@ import sqlite3
 conn = None
 COLUMNS = "USER_NM, STD_AMT, STD_CNT, LAST_LOGIN_HMS, LAST_OUT_HMS"
 
-
 def init_db(db_name, table_name):
     global conn
     conn = sqlite3.connect(db_name)
@@ -12,11 +11,8 @@ def init_db(db_name, table_name):
     c.execute(f"SELECT COUNT(*) FROM sqlite_master WHERE name='{table_name}';")
 
     if c.fetchone()[0] == 0:
-        c.execute(
-            f"CREATE TABLE {table_name}(USER_NM text, STD_AMT int, STD_CNT int, LAST_LOGIN_HMS timestamp, LAST_OUT_HMS timestamp);"
-        )
+        c.execute(f"CREATE TABLE {table_name}(USER_NM text, STD_AMT int, STD_CNT int, LAST_LOGIN_HMS timestamp, LAST_OUT_HMS timestamp);")
     conn.commit()
-
 
 def start_study(user_name, table_name, current_time):
     global conn
@@ -25,16 +21,14 @@ def start_study(user_name, table_name, current_time):
     datas = None
 
     c = conn.cursor()
-    c.execute(f"SELECT * FROM {table_name} WHERE USER_NM = ?", (user_name,))
+    c.execute(f"SELECT * FROM {table_name} WHERE USER_NM = ?", (user_name, ))
     rows = c.fetchall()
     if len(rows) == 0:
         query = f"INSERT INTO {table_name}({COLUMNS}) VALUES (?, ?, ?, ?, ?)"
         datas = [user_name, 0, 0, current_time, "NULL"]
         print(f"New User : {user_name}")
     else:
-        c.execute(
-            f"SELECT * FROM {table_name} WHERE USER_NM = ?", (user_name,)
-        )
+        c.execute(f"SELECT * FROM {table_name} WHERE USER_NM = ?", (user_name, ))
         query = f"UPDATE {table_name} SET LAST_LOGIN_HMS = ? WHERE USER_NM = ?"
         datas = [current_time, user_name]
 
@@ -42,7 +36,6 @@ def start_study(user_name, table_name, current_time):
     conn.commit()
 
     return None
-
 
 def end_study(user_name, table_name, current_time):
     global conn
@@ -53,26 +46,19 @@ def end_study(user_name, table_name, current_time):
     c = conn.cursor()
     c.execute(f"SELECT * FROM {table_name} WHERE USER_NM=?", (user_name,))
     rows = c.fetchall()[0]
-
+    
     if len(rows) == 0:
         print(f"New User : {user_name}")
         return -1
-
+    
     start_time = rows[3]
     elapsed_time = calculate_elapsed(start_time, current_time).total_seconds()
-    datas = (
-        rows[1] + elapsed_time,
-        rows[2] + 1,
-        "NULL",
-        current_time,
-        user_name,
-    )
+    datas = (rows[1]+elapsed_time, rows[2]+1, "NULL", current_time, user_name)
 
     c.execute(query, datas)
     conn.commit()
 
     return None
-
 
 def list_study(table_name):
     global conn
@@ -85,13 +71,11 @@ def list_study(table_name):
         results += str(row) + "\n"
     return results
 
-
 def close_db():
     global conn
     conn.close()
 
     return None
-
 
 def edit_time(user_name, edit_type):
     return None
--- utils/handle_time.py
+++ utils/handle_time.py
@@ -1,14 +1,10 @@
 from urllib import request
-from datetime import datetime, timedelta
-
+from datetime import datetime, timedelta 
 
 def get_current_time():
-    str_time = request.urlopen("https://www.naver.com").headers["Date"]
-    current_time = datetime.strptime(
-        str_time, "%a, %d %b %Y %H:%M:%S %Z"
-    ) - timedelta(hours=-9)
+    str_time = request.urlopen('https://www.naver.com').headers['Date']
+    current_time = datetime.strptime(str_time,'%a, %d %b %Y %H:%M:%S %Z') - timedelta(hours=-9) 
     return current_time.strftime("%Y-%m-%d %H:%M:%S")
-
 
 def calculate_elapsed(start_time, end_time):
     start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
@@ -16,20 +12,19 @@ def calculate_elapsed(start_time, end_time):
 
     return end_time - start_time
 
-
 def handle_timedelta(td):
     days = td.days
-    hours = td.seconds // 3600
-    minutes = (td.seconds // 60) % 60
+    hours = td.seconds//3600
+    minutes = (td.seconds//60)%60
     seconds = td.seconds % 60
-    return_string = ""
+    return_string = ''
     if days != 0:
-        return_string += f"{days}일 "
+        return_string += f'{days}일 '
     if hours != 0:
-        return_string += f"{hours}시간 "
+        return_string += f'{hours}시간 '
     if minutes != 0:
-        return_string += f"{minutes}분 "
+        return_string += f'{minutes}분 '
     if seconds != 0:
-        return_string += f"{seconds}초"
+        return_string += f'{seconds}초'
 
     return return_string
